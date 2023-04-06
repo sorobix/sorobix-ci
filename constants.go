36c91730cc086a125913cbc458bca8d5935df231
Hemanth Krishna <hkpdev008@gmail.com>
2023-04-07 05:26:54 +0530

    chore: default secrets
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- constants.go
+++ constants.go
@@ -4,6 +4,14 @@ import "os"
 
 var (
 	SCRIPT = "../run1.sh"
-	SECRET = os.Getenv("secret")
+	SECRET = getSecret()
 	DBNAME = "sorobix-ci-db"
 )
+
+func getSecret() string {
+	res := os.Getenv("secret")
+	if res != "" {
+		return res
+	}
+	return "sorobixci"
+}
